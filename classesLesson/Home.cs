febd90daf8fce49beab2477d4f38dde785c62f3a
Mammadov <tu201806281@aztu.local>
2021-10-29 09:24:18 +0400

    Classes L

---
+++ classesLesson/Home.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classesLesson
+{
+    class Home
+    {
+        public int otaq_say ;
+        byte mertebe_say ;
+        public string unvan ;
+        public string telefon ;
+        public string sheher;
+        public string method()
+        {
+            return $"{unvan}{telefon}{sheher}";
+        }
+    }
+   
+}
---
+++ classesLesson/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace classesLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Home a = new Home();
+            a.unvan= "Akim Abbasov kuce 7 \n";
+            a.telefon = "+994 55 111 11 11 \n";
+            a.sheher = "Baku";
+            Console.WriteLine(a.method());
+        }
+
+    }
+}
